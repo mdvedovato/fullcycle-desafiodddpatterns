99283a797132a55412d8bc3031e45f12782e247a
mdvedovato <mdvedovato@gmail.com>
2024-08-15 08:03:22 -0300

    Retirado devido Revisão Necessária

--- src/domain/event/customer-address-changed.event.ts
+++
@@ -1,8 +0,0 @@
-export default class CustomerAddressChangedEvent {
-    constructor(
-      public readonly customerId: string,
-      public readonly customerName: string,
-      public readonly address: string
-    ) {}
-  }
-  
--- src/domain/event/handler/envia-console-log.handler.ts
+++
@@ -1,9 +0,0 @@
-import CustomerAddressChangedEvent from "../customer-address-changed.event";
-
-export default class EnviaConsoleLogHandler {
-  handle(event: CustomerAddressChangedEvent): void {
-    console.log(
-      `Endereço do cliente: ${event.customerId}, ${event.customerName} alterado para: ${event.address}`
-    );
-  }
-}
